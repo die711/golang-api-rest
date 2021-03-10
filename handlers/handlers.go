9a5461764778ab3cae0f5bb6c8febbbda4ab1e56
Diego Resendiz Ojeda <di_564@hotmail.com>
2021-03-10 00:23:10 -0800

    cambios handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"rest/models"
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +12,9 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Se obtiene un usuario")
+	user := models.User{Id: 1, Username: "diego", Password: "123456"}
+	output, _ := json.Marshal(&user)
+	fmt.Fprintf(w, string(output))
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
