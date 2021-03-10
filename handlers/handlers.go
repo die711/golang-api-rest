4e486b7015f91cd2662364fe7aee5379b79d300d
Diego Resendiz Ojeda <di_564@hotmail.com>
2021-03-09 23:54:32 -0800

    cambios handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,2 +1,26 @@
 package handlers
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Se obtiene todos los usuarios")
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Se obtiene uno usuario")
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Se crea un usuario")
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Se actualiza un usuario")
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Se elimina un usuario")
+}
--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"rest/handlers"
 )
 
 func main() {
 
 	mux := mux.NewRouter()
 
-	mux.HandleFunc("/api/v1/users/").Methods("GET")
-	mux.HandleFunc("/api/v1/users/{id:[0-9]+}").Methods("GET")
-	mux.HandleFunc("/api/v1/users/").Methods("POST")
-	mux.HandleFunc("/api/v1/users/{id:[0-9]+}").Methods("PUT")
-	mux.HandleFunc("/api/v1/users/{id:[0-9]+}").Methods("DELETE")
+	mux.HandleFunc("/api/v1/users/", handlers.GetUsers).Methods("GET")
+	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.GetUser).Methods("GET")
+	mux.HandleFunc("/api/v1/users/", handlers.UpdateUser).Methods("POST")
+	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
+	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
 
 	log.Println("El servidor ala esucha en el puerto 8000")
 	log.Fatal(http.ListenAndServe(":8000", mux))
