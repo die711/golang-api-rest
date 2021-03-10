ae0e0d59499317080c3691c9fe86ddd079fbb5d5
Diego Resendiz Ojeda <di_564@hotmail.com>
2021-03-09 23:58:33 -0800

    cambios handlers

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	mux.HandleFunc("/api/v1/users/", handlers.UpdateUser).Methods("POST")
 	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
 	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
+	mux.HandleFunc("/api/v1/users/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
 
 	log.Println("El servidor ala esucha en el puerto 8000")
 	log.Fatal(http.ListenAndServe(":8000", mux))
